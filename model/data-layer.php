330ef722741ad6b68bf0f1283137d82d32bb4f0e
HansenBlakestudentgreenriveredu <hansen.blake@student.greenriver.edu>
2024-05-02 12:40:30 -0700

    Created folders and model validation

---
+++ model/data-layer.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Data layer.
+ * It belongs to the model.
+ */
+
+// Get meals
+function getMeals() {
+    return array('chicken', 'burger', 'salad');
+}
+
+// Get sides
+function getSides() {
+    return array('fries', 'rice', 'fruit');
+}
+
+// Get drinks
+function getDrinks() {
+    return array('coke', 'water', 'juice');
+}
