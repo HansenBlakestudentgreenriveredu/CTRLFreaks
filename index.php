7d79e2e46423d518fae270acfb6a2a525bd1ee63
Tilak-21 <tilakamlesh2112@gmail.com>
2024-05-28 13:05:52 -0400

    index changes to routing

--- index.php
+++ index.php
@@ -2,32 +2,40 @@
 // Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
 // Require the autoload file
-require_once ('vendor/autoload.php');
-require_once ('model/data-layer.php');
-require_once ('model/validate.php');
+require_once('vendor/autoload.php');
+require_once('model/data-layer.php');
+require_once('model/validate.php');
+
 // Instantiate the F3 Base class
 $f3 = Base::instance();
+
 // Define a default route
 $f3->route('GET /', function() {
     // Render a view page
     $view = new Template();
     echo $view->render('views/index.html');
 });
-// home route
+
+// Home route
 $f3->route('GET /home', function() {
     // Render a view page
     $view = new Template();
     echo $view->render('views/index.html');
 });
-// route to menu page
-$f3->route('GET /menu', function() {
+
+// Route to menu page
+$f3->route('GET /menu', function($f3) {
+    $menuItems = getMenuItems(); // Assuming getMenuItems() is a function in data-layer.php
+    $f3->set('menuItems', $menuItems);
+
     // Render a view page
     $view = new Template();
     echo $view->render('views/menu.html');
 });
 
-// route to summary page
+// Route to summary page
 $f3->route('GET /orderSummary', function() {
     // Render a view page
     $view = new Template();
