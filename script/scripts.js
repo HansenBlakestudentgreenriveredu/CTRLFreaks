88f9ed6e32b44f082d72b3362d4136d67f22c84d
Vlad Oleksiyenko <vladolek2003@gmail.com>
2024-05-30 01:48:17 -0400

    Added some styles to fix the cart vertical responsiveness

--- script/scripts.js
+++ script/scripts.js
@@ -10,15 +10,30 @@
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    goToTopBtn.style.display = "block";
-} else {
-    goToTopBtn.style.display = "none";
-}
-}
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopBtn.style.display = "block";
+        } else {
+            goToTopBtn.style.display = "none";
+        }
+    }
 
     // When the user clicks on the button, scroll to the top of the document
     function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+    let activeUserNotificationStatus = document.querySelector('.active-choice');
+    let nonActiveStatus = document.querySelector(".not-active");
+
+    console.log(nonActiveStatus);
+    console.log(activeUserNotificationStatus);
+
+    nonActiveStatus.addEventListener("mousedown", function() {
+        confirm("Are you sure you want to change your notification status?");
+        nonActiveStatus.className = "active-choice";
+        activeUserNotificationStatus.className = "not-active";
+    });
+
+
+
